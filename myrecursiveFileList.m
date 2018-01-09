758a4a421330ceb3ac70cb36d0f4f6bbe8ff61f5
sbkill1r <2386997963@qq.com>
2018-01-09 15:15:43 +0800

    上传文件读取函数

---
+++ myrecursiveFileList.m
@@ -0,0 +1,39 @@
+function allData=myrecursiveFileList(directoryList, ext)
+% RecursiveFileList List files with a given extension recursively
+
+%	 20030316
+
+if nargin<2, ext='wav'; end
+if nargin<1, error('Need at least one input argument!'); end
+if isstr(directoryList); directoryList={directoryList}; end
+
+allData=[];
+for k=1:length(directoryList)
+	directory=directoryList{k};
+
+	% Get files in the given directory
+	if (directory(end)=='/') | (directory(end)=='\'); directory(end)=[]; end
+
+	data=dir([directory, '/*.', ext]);
+
+	for i=1:length(data)
+		data(i).path=[directory, '/', data(i).name];
+		[parentPath, junk, junk]=fileparts(data(i).path);
+		[junk, data(i).parentDir, junk]=fileparts(parentPath);
+	end
+	
+	% Get files in sub-directories
+	subdirs=dir(directory);
+	subdirs=subdirs([subdirs.isdir]);	% Get directories only
+	for i=1:length(subdirs),
+		if strcmp(subdirs(i).name, '.'), continue; end
+		if strcmp(subdirs(i).name, '..'), continue; end
+		thisPath=[directory, '/', subdirs(i).name];
+		data2=feval(mfilename, thisPath, ext);
+		if length(data2)==0; data2=[]; end
+		if length(data)==0; data=[]; end
+		data=[data; data2];
+	end
+	allData=[allData; data];
+end
+
